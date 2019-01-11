7904103bb4681a2637f2de46e41d79a9f76feb50
Heloise Taillet <ht2394@columbia.edu>
2019-01-11 17:42:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
 
+class CashRegister
+  
+  attr_accessor :total, :discount
+  attr_writer :discount
+  
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total += price*quantity
+  end
+  
+  def apply_discount
+    if @discount != 0
+   (self.total.to_f*(1.0-(self.discount.to_f/100))).to_f
+ end
+  end
+
+  def void_last_transaction
+  end
+end
