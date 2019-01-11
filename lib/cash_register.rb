769c712bca7b21b4cc198ab1c05b0f36c956b1bb
Heloise Taillet <ht2394@columbia.edu>
2019-01-11 17:53:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,11 +4,11 @@ class CashRegister
   
   attr_accessor :total, :discount
   
-      @@all  = []
 
   def initialize(discount = 0)
     @total = 0 
     @discount = discount
+    @@all  = []
   end
 
   def add_item(title, price, quantity = 1)
